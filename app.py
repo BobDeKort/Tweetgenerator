908f76709cae0258f21479b8e7098cb558312729
BobDeKort <dekort.bob101@gmail.com>
2016-11-13 21:20:39 -0800

    host specified

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 import random
+import os
 app = Flask(__name__)
 filePath = 'text/sampleText.txt'
 listOfChars = [',', '"', ',', '.', '?', '!', ':', ';', '-', '/']
@@ -78,4 +79,5 @@ def getTotalAmountOfWords(histogram):
     return total
 
 if __name__ == "__main__":
-    app.run(host='0.0.0.0', port=port)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(debug=True, host='0.0.0.0', port=port)
